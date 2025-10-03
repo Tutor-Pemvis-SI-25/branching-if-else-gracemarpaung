eb6ff2aa805679b61778bd46279f07ff59595157
gracemarpaung <gracelusyanna34@gmail.com>
2025-10-03 15:32:07 +0700

    Pair21
    Delll

---
+++ T00.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.Math;
+
+public class T00 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x, y, hasilpenjumlahan, hasilperkalian;
+
+        x = Integer.parseInt(input.nextLine());
+        y = Integer.parseInt(input.nextLine());
+        if (x % 2 == 0) {
+            System.out.println("bilangan pertama: genap");
+        } else {
+            System.out.println("bilangan pertama: ganjil");
+        }
+        if (y % 2 == 0) {
+            System.out.println("bilangan kedua: genap");
+        } else {
+            System.out.println("bilangan kedua: ganjil");
+        }
+        if (x > y) {
+            System.out.println("bilangan pertama lebih besar");
+        } else {
+            if (y > x) {
+                System.out.println("kedua bilangan kedua lebih besar");
+            } else {
+                System.out.println("kedua bilangan sama besar");
+            }
+        }
+        if (x % 2 == 0 && y % 2 == 0) {
+            hasilpenjumlahan = x + y;
+            System.out.println("hasil penjumlahan " + hasilpenjumlahan);
+        } else {
+            if (x % 2 != 0 && y % 2 != 0) {
+                hasilperkalian = x * y;
+                System.out.println("hasil perkalian:" + hasilperkalian);
+            } else {
+                System.out.println("Bebas jenis");
+            }
+        }
+    }
+}
